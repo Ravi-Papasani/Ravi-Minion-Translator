f1059c02234d974f723d1a38bc8071c24dfa6248
Ravi-Papasani <76274650+Ravi-Papasani@users.noreply.github.com>
2020-12-29 23:26:29 -0600

    practice things to create App

--- app.js
+++ app.js
@@ -9,3 +9,33 @@ function clickEventHandler() {
 };
 
 btnTranslator.addEventListener("click", clickEventHandler);
+
+
+var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+//Using JavaScript Browser console getting API response
+
+var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=I am IronMan"
+
+fetch(url) //click shift + enter to visit next line
+.then(response=>response.json())
+.then(json=>console.log(json))
+
+
+
+//PostMan Call
+HttpMethod: GET
+https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=I am IronMan
+
+output:
+
+{
+    "success": {
+        "total": 1
+    },
+    "contents": {
+        "translated": "Testing,  you are",
+        "text": "I am IronMan",
+        "translation": "yoda"
+    }
+}
