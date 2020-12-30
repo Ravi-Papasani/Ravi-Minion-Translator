1e07fbc2d489fc7799d4d718f4da07117f5e7279
Ravi-Papasani <76274650+Ravi-Papasani@users.noreply.github.com>
2020-12-30 00:41:05 -0600

    error in Request Url construction

--- app.js
+++ app.js
@@ -2,40 +2,26 @@ var btnTranslator = document.querySelector("#btn-translator");
 var textInput = document.querySelector("#txt-input");
 var outPutDiv = document.querySelector("#output");
 
+var serverURl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+//  var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+function getTranslatorURL(text) {
+    return serverURl + "?" + "text=" + text
+}
 
 function clickEventHandler() {
-    outPutDiv.innerText = "acsdtfadfuasgj " + textInput.value;
+    //taking input from user
+    var inputText = textInput.value;
+
+    //processing the input by calling the server to fetch data
+    fetch(getTranslatorURL(textInput))
+        .then(response => response.json())
+        .then(json => console.log(json))
+
+    //output the response
+
+    // outPutDiv.innerText = "acsdtfadfuasgj " + textInput.value;
 
 };
 
 btnTranslator.addEventListener("click", clickEventHandler);
-
-
-var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-
-//Using JavaScript Browser console getting API response
-
-var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=I am IronMan"
-
-fetch(url) //click shift + enter to visit next line
-.then(response=>response.json())
-.then(json=>console.log(json))
-
-
-
-//PostMan Call
-HttpMethod: GET
-https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=I am IronMan
-
-output:
-
-{
-    "success": {
-        "total": 1
-    },
-    "contents": {
-        "translated": "Testing,  you are",
-        "text": "I am IronMan",
-        "translation": "yoda"
-    }
-}
