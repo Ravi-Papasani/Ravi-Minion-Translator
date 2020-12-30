66144a7c9b77fdfc620aecc780763c0100da6980
Ravi-Papasani <76274650+Ravi-Papasani@users.noreply.github.com>
2020-12-29 23:04:37 -0600

    input processing output

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-//button tag used id as attribute then .queryselector has #
 var btnTranslator = document.querySelector("#btn-translator");
-console.log(btnTranslator);
-
 var textInput = document.querySelector("#txt-input");
+var outPutDiv = document.querySelector("#output");
 
 
 function clickEventHandler() {
-    console.log("you clicked me");
-    console.log("Input " + textInput.value);
+    outPutDiv.innerText = "acsdtfadfuasgj " + textInput.value;
+
 };
 
-//Hey Button when I clicked, perform call back function, and that function is clickEventHandler() here
 btnTranslator.addEventListener("click", clickEventHandler);
